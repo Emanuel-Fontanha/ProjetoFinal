64b05ba4c5e89cc8acd330e09085909270fb0d05
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-03-06 20:15:38 -0300

    feat: add funcionalidade do botão de ir para a pagina de feedback

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -6,6 +6,7 @@ import contatosicone from "../../assets/people.png";
 import entregaicone from "../../assets/delivery.png";
 import PijamaCard from "../../components/pijama-card";
 import FeedbackCarrossel from "../../components/feedback-carrossel";
+import { Link } from "react-router-dom";
 
 const feedback = { nota: 4, nome: "Fulano de Tal", feedback: "Lorem ipsum dolor sit amet. Et voluptatem officia ad sint voluptate qui  voluptas sunt non fugiat labore et consequatur voluptatem sed optio  veniam aut perferendis delectus! Aut Quis impedit a quas animi 33 alias  provident et ipsum deleniti eos pariatur quibusdam."}
 
@@ -50,10 +51,10 @@ export default function HomePage() {
             <div className={styles.promocoes}>
             <h1 className={styles.text3}>Nossas últimas promoções!</h1>
                 <div className={styles.cards}>
-                    <PijamaCard id={1} desconto={true} preco={67.06} nome="Pijama feminino longo - estampa poá" precoAntigo={78.90} favorito={false}/>
-                    <PijamaCard id={2} desconto={true} preco={67.06} nome="Pijama feminino longo - estampa poá" precoAntigo={78.90} favorito={false}/>
-                    <PijamaCard id={3} desconto={false} preco={78.90} nome="Pijama feminino longo - estampa poá" favorito={false}/>
-                    <PijamaCard id={4} desconto={false} preco={78.90} nome="Pijama feminino longo - estampa poá" favorito={false}/>
+                    <PijamaCard id={1} desconto={true} preco={67.06} nome="Pijama feminino longo - estampa poá" precoAntigo={78.90}/>
+                    <PijamaCard id={2} desconto={true} preco={67.06} nome="Pijama feminino longo - estampa poá" precoAntigo={78.90}/>
+                    <PijamaCard id={3} desconto={false} preco={78.90} nome="Pijama feminino longo - estampa poá"/>
+                    <PijamaCard id={4} desconto={false} preco={78.90} nome="Pijama feminino longo - estampa poá"/>
                 </div>
             </div>
             <div className={styles.feedbacks}>
@@ -61,9 +62,11 @@ export default function HomePage() {
                 <div className={styles.feedCarrossel}>
                     <FeedbackCarrossel cards={[feedback,feedback,feedback,feedback,feedback]} />
                 </div>
-                <button className={styles.button}>
-                    Também quero dar um feedback!
-                </button>
+                <Link to='/feedback' className={styles.link}>
+                    <button className={styles.button}>
+                        Também quero dar um feedback!
+                    </button>
+                </Link>
             </div>
         </div>
     );
