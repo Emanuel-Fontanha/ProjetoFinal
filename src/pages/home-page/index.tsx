88bcce47a5033281ab3d6e962e67af0c8e71c18b
Emanuel Fontanha <emanuellira@id.uff.br>
2026-03-05 21:09:40 -0300

    refactor: correção simples de formatação

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -7,58 +7,58 @@ import entregaicone from "../../assets/delivery.png";
 import PijamaCard from "../../components/pijama-card";
 
 export default function HomePage() {
-  return (
-    <div className={styles.homePage}>
-      <div className={styles.apresentacao}>
-        <figure>
-          <img src={logo} className={styles.logo} />
-        </figure>
-        <p className={styles.text1}>
-          Se os lobos soubessem desse conforto, nem sopravam casas, iam dormir!
-        </p>
-      </div>
-      <Carrossel />
-        <div className={styles.propaganda}>
-          <div className={styles.publicidade}>
-            <figure>
-              <img src={pijamaicone} className={styles.icon} />
-            </figure>
-            <p className={styles.text2}>
-              Pijamas confortáveis e com tecnologia
-            </p>
-          </div>
-          <div className={styles.publicidade}>
-            <figure>
-              <img src={contatosicone} className={styles.icon} />
-            </figure>
-            <p className={styles.text2}>
-              Modelos para todas as idades e tamanhos
-            </p>
-          </div>
-          <div className={styles.publicidade}>
-            <figure>
-              <img src={entregaicone} className={styles.icon} />
-            </figure>
-            <p className={styles.text2}>
-              Frete grátis em todo o Brasil e exterior
-            </p>
-          </div>
+    return (
+        <div className={styles.homePage}>
+            <div className={styles.apresentacao}>
+                <figure>
+                    <img src={logo} className={styles.logo} />
+                </figure>
+                <p className={styles.text1}>
+                    Se os lobos soubessem desse conforto, nem sopravam casas, iam dormir!
+                </p>
+            </div>
+            <Carrossel />
+            <div className={styles.propaganda}>
+                <div className={styles.publicidade}>
+                    <figure>
+                        <img src={pijamaicone} className={styles.icon} />
+                    </figure>
+                    <p className={styles.text2}>
+                        Pijamas confortáveis e com tecnologia
+                    </p>
+                </div>
+                <div className={styles.publicidade}>
+                    <figure>
+                        <img src={contatosicone} className={styles.icon} />
+                    </figure>
+                    <p className={styles.text2}>
+                        Modelos para todas as idades e tamanhos
+                    </p>
+                </div>
+                <div className={styles.publicidade}>
+                    <figure>
+                        <img src={entregaicone} className={styles.icon} />
+                    </figure>
+                    <p className={styles.text2}>
+                        Frete grátis em todo o Brasil e exterior
+                    </p>
+                </div>
+            </div>
+            <div className={styles.promocoes}>
+                <h1 className={styles.text3}>Nossas últimas promoções!</h1>
+                <div className={styles.cards}>
+                    <PijamaCard id={1} desconto={false} preco={67.50} />
+                    <PijamaCard id={2} desconto={false} preco={67.50} />
+                    <PijamaCard id={3} desconto={false} preco={67.50} />
+                    <PijamaCard id={4} desconto={false} preco={67.50} />
+                </div>
+            </div>
+            <div className={styles.feedbacks}>
+                <h1 className={styles.text3}>Feedbacks</h1>
+                <button className={styles.button}>
+                    Também quero dar um feedback!
+                </button>
+            </div>
         </div>
-        <div className={styles.promocoes}>
-          <h1 className={styles.text3}>Nossas últimas promoções!</h1>
-          <div className={styles.cards}>
-            <PijamaCard id={1} desconto={false} preco={67.50}/>
-            <PijamaCard id={2} desconto={false} preco={67.50}/>
-            <PijamaCard id={3} desconto={false} preco={67.50}/>
-            <PijamaCard id={4} desconto={false} preco={67.50}/>
-          </div>
-        </div>
-        <div className={styles.feedbacks}>
-          <h1 className={styles.text3}>Feedbacks</h1>
-          <button className={styles.button}>
-            Também quero dar um feedback!
-          </button>
-        </div>
-    </div>
-  );
+    );
 }
