a432c8dc9636f015d425d0d839e51a8b26a69c2f
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-03-06 16:24:56 -0300

    fix: consertando componente de pijama card pra funcionar o favorito

--- src/components/pijama-card/index.tsx
+++ src/components/pijama-card/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.css";
 import icone from '../../assets/iconeDisconto.png'
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 interface pijamaCard{
   id: number
@@ -8,19 +9,25 @@ interface pijamaCard{
   nome?: string
   preco: number
   precoAntigo?: number
+  favorito: boolean
   desconto: boolean
 }
 
-export default function PijamaCard(props : pijamaCard) {
+export default function PijamaCard(props: pijamaCard) {
+  const [favorito, setFavorito] = useState(props.favorito)
+
   const desconto = props.desconto;
+
+  function handleFavorite() {
+    setFavorito(!favorito)
+  }
 
   return (
     <div className={styles.pijamaCard}>
       <div className={styles.fotoCard}>
         <div className={styles.header}>
           <div>
-            <input type="checkbox" id="checkbox" className={styles.check} />
-            <label htmlFor="checkbox" className={styles.label}>
+            <button className={styles.button} onClick={handleFavorite}>
               <svg
                 width="67"
                 height="70"
@@ -30,16 +37,16 @@ export default function PijamaCard(props : pijamaCard) {
               >
                 <path
                   d="M10.3705 23.8838C16.0979 17.8879 25.3838 17.8879 31.1112 23.8838L53.926 47.768L33.1853 69.481L10.3705 45.5967C4.64308 39.6009 4.64308 29.8796 10.3705 23.8838V23.8838Z"
-                  fill="#A4A8B0"
+                  fill={favorito ? "#A31621" : "#A4A8B0"}
                   className={styles.coracao}
                 />
                 <path
                   d="M55.9999 23.884C61.7273 29.8799 61.7273 39.6011 55.9999 45.597L33.1851 69.4812L12.4443 47.7683L35.2591 23.884C40.9865 17.8881 50.2725 17.8881 55.9999 23.884V23.884Z"
-                  fill="#A4A8B0"
+                  fill={favorito ? "#A31621" : "#A4A8B0"}
                   className={styles.coracao}
                 />
               </svg>
-            </label>
+            </button>
           </div>
           {desconto && <img src={icone} className={styles.discountIcon}/>}
         </div>
